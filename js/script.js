96c4bea6b92d05d8b65445b823b3ed47761fb1a2
Luca-Favaretto <145663602+Luca-Favaretto@users.noreply.github.com>
2023-11-06 15:49:31 +0100

    .

---
+++ js/script.js
@@ -0,0 +1,78 @@
+class User {
+  constructor(firstname, lastName, age, location) {
+    this.firstname = firstname;
+    this.lastName = lastName;
+    this.age = age;
+    this.location = location;
+  }
+  static compareAge(userA, userB) {
+    if (userA.age > userB.age) {
+      console.log("il primo è più grande del secondo");
+    } else {
+      console.log("il secondo è più grande del primo");
+    }
+  }
+  isSameAge(userCompare) {
+    if (this.age > userCompare.age) {
+      console.log("il primo è più grande del secondo");
+    } else {
+      console.log("il secondo è più grande del primo");
+    }
+  }
+}
+const users = [
+  new User("Mario", "Rossi", 15, "Verona"),
+  new User("Luca", "Verdi", 25, "Milano"),
+  new User("Marta", "Neri", 16, "Venezia"),
+  new User("Giulia", "Blu", 19, "Roma")
+];
+
+User.compareAge(users[1], users[0]);
+users[0].isSameAge(users[2]);
+
+//
+//
+//
+//
+
+let formArray = [];
+const form = document.querySelector("form");
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  let petName = document.getElementById("pet-name").value;
+  let ownerName = document.getElementById("owner-name").value;
+  let species = document.getElementById("species").value;
+  let breed = document.getElementById("breed").value;
+
+  let formElement = [petName, ownerName, species, breed];
+  formArray.push(formElement);
+
+  document.getElementById("pet-name").value = "";
+  document.getElementById("owner-name").value = "";
+  document.getElementById("species").value = "";
+  document.getElementById("breed").value = "";
+
+  console.log(formArray);
+});
+
+class Pet {
+  constructor(patName, ownerName, species, breed) {
+    this.patName = patName;
+    this.ownerName = ownerName;
+    this.specieas = species;
+    this.breed = breed;
+  }
+  static sameOwner(petA, petB) {
+    if (petA.ownerName === petB.ownerName) {
+      console.log("I due animali hanno lo stesso proprietario.");
+    } else {
+      console.log("I due animali hanno proprietari differneti.");
+    }
+  }
+}
+
+const pets = [];
+
+const petObject = function () {
+  formArray.forEach(pet => pets.push(new Pet(pet[0], pet[1], pet[2], pet[3])));
+};
